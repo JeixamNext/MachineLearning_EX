e275edd8024d3e68cf0640e308a0e6e33a3ebaa6
jeixam <jaime.rodriguez@viewnext.com>
2022-08-31 13:37:38 +0200

    first commit

---
+++ scan_version.py
@@ -0,0 +1,13 @@
+Import scipy 
+Import numpy
+Import matplotlib
+Import pandas
+Import statsmodels
+Import skalern  
+
+print('scipy: %s' % scipy.__version__) 
+print('numpy: %s' % numpy.__version__) 
+print('matplotlib: %s' % matplotlib.__version__) 
+print('pandas: %s' % pandas.__version__) 
+print('statsmodels: %s' % statsmodels.__version__) 
+print('skalern: %s' % skalern.__version__) 
---
+++ version_deep.py
@@ -0,0 +1,5 @@
+Import tensorflow
+Import keras
+ 
+print('tensorflow: %s' % tensorflow.__version__) 
+print('keras: %s' % keras.__version__) 
